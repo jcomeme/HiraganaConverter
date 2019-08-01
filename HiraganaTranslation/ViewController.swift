f021a7c545be0bee190b23dfe0356fcbf815d6e7
jcomeme <hara@sunyou.co.jp>
2019-08-01 10:41:26 +0900

    Initial Commit

---
+++ HiraganaTranslation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiraganaTranslation
+//
+//  Created by YoshinobuHARA on 2019/08/01.
+//  Copyright © 2019 OmemeAnkohji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
